8b9fa46ea16631d8b75554cff6370ae3c8e662f4
pngdrift <mr.aivazlatypov@yandex.ru>
2022-01-25 20:47:05 +0500

    upd

---
+++ rules/br/js/HTML5Domains.js
@@ -0,0 +1,5 @@
+var HTML5Domains = [
+    "web-temp.tankionline.com", // Домен в песочнице, нужен для тестов
+    "tntgames.tankionline.com"
+];
+var HTML5Percentage = 5; // Количество процентов игроков для редиректа на HTML5
